4d3550060c574f78dff2fd324fdb8f0774ed85ee
tingamapuro04 <alponcemobutu100@gmail.com>
2022-08-20 12:04:47 +0300

    javascript

--- index.js
+++ index.js
@@ -5,14 +5,23 @@
 // mo.addEventListener('click', kilifi);
 
 window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return
+  audio.currentTime = 0; // Rewind play to the start;
   audio.play();
-})
+  key.classList.add("playing")
+});
+
+function endTransition(e) {
+  if (e.propertyName !== 'transform') return
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", endTransition))
 
 // window.addEventListener('click', function(e) {
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-//   if(!audio) return
-//   audio.play();
-//   console.log("ty")
+//   const audio = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+//   console.log(audio)
 // })
